c62385a20a31a3ee8773f7c74038fd6ab9a82797
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-06-27 21:26:43 -0700

    Update the output
    Change the output to a list of possible next states.

--- algorithm/pw.py
+++ algorithm/pw.py
@@ -1,10 +1,8 @@
 import math
 from anytree import AnyNode as Node
 import numpy as np
-#print(math.floor(3.9))
 
-#print(pow(2,3))
-class FindNextState1:
+class FindNextState:
     def __init__(self, alpha, transitionFunction):
         self.alpha = alpha
         self.transitionFunction = transitionFunction
@@ -18,25 +16,9 @@ class FindNextState1:
         if k > len(actionNode.children):
             if nextState not in actionNode.children:
             	nextStateNode = Node(parent = actionNode, id = nextState, numVisited =1)## add action
-        return actionNode.children
+        nextStates = [action.id for action in actionNode.children]
+        return nextStates
 
 def transitionFunction(state, action):
     nextState = np.array(state)+np.array(action)
     return list(nextState)
-
-#print(transitionFunction([0,1],[1,2]))
-state = Node(id = [0,1], numVisited =1)
-action = Node(id = [1,2], numVisited=1, parent = state)
-#n2 = Node(id = [1,3], numVisited=1, parent = action)
-n3 = Node(id = [2,4], parent = action)
-g = Node(id = [2,5], parent = action)
-fk = FindNextState1(0, transitionFunction)
-print(fk(state, action))
-'''
-root = AnyNode(id="root")
-k = AnyNode(parent = root)
-print(RenderTree(root))
-print(len(root.children))
-if k in root.children:
-    print("a")
-'''
