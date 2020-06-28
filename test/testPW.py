07dd8ff713122369a1946a6a26a81521bfe42e48
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-06-27 21:38:44 -0700

    Update tests of new branches
    Update tests of new branches, when k<numVisited, and when action has other child nodes.

--- test/testPW.py
+++ test/testPW.py
@@ -7,20 +7,41 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from algorithm.pw import FindNextState1, transitionFunction
+from algorithm.pw import FindNextState, transitionFunction
 @ddt
 class TestMCTS(unittest.TestCase):
     def setUp(self):
     	self.alpha = 0.5
 
-    @data(([1,1],[1,0],[0,1],[1,1]))
+    @data(([1,1],[1,0],[0,1],[[1,1]]), ([1,1],[1,2],[0,1],[[1,3]]))
     @unpack
-    def testNextState(self, numVisted, state, action, nextSate):
-    	findNextSate = FindNextState1(self.alpha, transitionFunction)
+    def testNextState(self, nodeNumVisted, state, action, nextSate):
+    	stateNode = Node(id = state, numVisited = nodeNumVisted[0])
+    	actionNode = Node(id = action, numVisited = nodeNumVisted[1], parent = stateNode)
+    	findNextSate = FindNextState(self.alpha, transitionFunction)
+    	truthNextState = findNextSate(stateNode, actionNode)
+    	self.assertAlmostEqual(truthNextState, nextSate)
 
-    	trueNextState = findNextSate(state, action)
-    	self.assertAlmostEqual(trueNextState, nextSate, places=1) 
+    @data(([1,1],[1,0],[0,1], [[1,3]]), ([1,1],[1,2],[0,1],[[1,3]]))
+    @unpack
+    def testNextState2(self, nodeNumVisted, state, action, nextSate):
+    	stateNode = Node(id = state, numVisited = nodeNumVisted[0])
+    	actionNode = Node(id = action, numVisited = nodeNumVisted[1], parent = stateNode)
+    	stateChild = Node(id = [1,3], parent = actionNode)
+    	findNextSate = FindNextState(self.alpha, transitionFunction)
+    	truthNextState = findNextSate(stateNode, actionNode)
+    	self.assertAlmostEqual(truthNextState, nextSate)
+
+    @data(([1,1],[1,0],[0,1], [[1,3], [1,5]]))
+    @unpack
+    def testNextState3(self, nodeNumVisted, state, action, nextSate):
+    	stateNode = Node(id = state, numVisited = nodeNumVisted[0])
+    	actionNode = Node(id = action, numVisited = nodeNumVisted[1], parent = stateNode)
+    	stateChild = Node(id = [1,3], parent = actionNode)
+    	stateChild2 = Node(id = [1,5], parent = actionNode)    	
+    	findNextSate = FindNextState(self.alpha, transitionFunction)
+    	truthNextState = findNextSate(stateNode, actionNode)
+    	self.assertAlmostEqual(truthNextState, nextSate) 
 
 if __name__ == '__main__':
     unittest.main()
-
