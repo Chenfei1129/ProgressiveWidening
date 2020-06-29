edf2124e5d3752edc642953bc7a7a30fe769ad14
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-06-29 12:46:43 -0700

    Add select action function

--- algorithm/pw.py
+++ algorithm/pw.py
@@ -2,6 +2,38 @@ import math
 from anytree import AnyNode as Node
 import numpy as np
 
+class ScoreChild:
+    def __init__(self, cInit, cBase):
+        self.cInit = cInit
+        self.cBase = cBase
+    def __call__(self, stateNode, actionNode):
+        stateActionVisitCount = actionNode.numVisited
+        stateVisitCount = stateNode.numVisited
+        actionPrior = actionNode.actionPrior
+        if stateActionVisitCount == 0:
+            uScore = np.inf
+            qScore = 0 
+        else:
+            explorationRate = np.log((1 + stateVisitCount + self.cBase) / self.cBase) + self.cInit
+            uScore = explorationRate * actionPrior * np.sqrt(stateVisitCount) / float(1 + stateActionVisitCount)
+            nextStateValues = [nextState.sumValue for nextState in actionNode.children]
+            sumNextStateValues = sum(nextState for nextState in nextStateValues)
+            qScore = sumNextStateValues / stateActionVisitCount
+
+        score = qScore + uScore
+        return score   
+
+class SelectAction:
+    def __init__(self, calculateScore):
+        self.calculateScore = calculateScore
+
+    def __call__(self, stateNode, actionNode):
+        scores = [self.calculateScore(stateNode, actionNode) for actionNode in stateNode.children]
+        maxIndex = np.argwhere(scores == np.max(scores)).flatten()
+        selectedChildIndex = np.random.choice(maxIndex)
+        selectedChild = stateNode.children[selectedChildIndex]
+        return selectedChild
+
 class FindNextState:
     def __init__(self, alpha, transitionFunction):
         self.alpha = alpha
@@ -22,22 +54,3 @@ class FindNextState:
 def transitionFunction(state, action):
     nextState = np.array(state)+np.array(action)
     return list(nextState)
-
-class ScoreChild:
-    def __init__(self, cInit, cBase):
-        self.cInit = cInit
-        self.cBase = cBase
-    def __call__(self, stateNode, actionNode, actionNodeChild):
-        stateActionVisitCount = actionNode.numVisited
-        stateVisitCount = stateNode.numVisited
-        actionPrior = actionNode.actionPrior
-        if stateActionVisitCount == 0:
-            uScore = np.inf
-            qScore = 0 
-        else:
-            explorationRate = np.log((1 + stateVisitCount + self.cBase) / self.cBase) + self.cInit
-            uScore = explorationRate * actionPrior * np.sqrt(stateVisitCount) / float(1 + stateActionVisitCount)#selfVisitCount is stateACtionVisitCount
-            qScore = actionNodeChild.sumValue / stateActionVisitCount
-
-        score = qScore + uScore
-        return score    
