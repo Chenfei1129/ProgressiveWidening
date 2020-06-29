82a39795571064cdb372745173f5498b393b1280
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-06-29 12:04:41 -0700

    Add ScoreChild function (actions become nodes)

--- algorithm/pw.py
+++ algorithm/pw.py
@@ -22,3 +22,22 @@ class FindNextState:
 def transitionFunction(state, action):
     nextState = np.array(state)+np.array(action)
     return list(nextState)
+
+class ScoreChild:
+    def __init__(self, cInit, cBase):
+        self.cInit = cInit
+        self.cBase = cBase
+    def __call__(self, stateNode, actionNode, actionNodeChild):
+        stateActionVisitCount = actionNode.numVisited
+        stateVisitCount = stateNode.numVisited
+        actionPrior = actionNode.actionPrior
+        if stateActionVisitCount == 0:
+            uScore = np.inf
+            qScore = 0 
+        else:
+            explorationRate = np.log((1 + stateVisitCount + self.cBase) / self.cBase) + self.cInit
+            uScore = explorationRate * actionPrior * np.sqrt(stateVisitCount) / float(1 + stateActionVisitCount)#selfVisitCount is stateACtionVisitCount
+            qScore = actionNodeChild.sumValue / stateActionVisitCount
+
+        score = qScore + uScore
+        return score    
