17df57d83cf6154fb3d22f3abd6dd21ea7f0977d
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-07-04 13:08:04 -0700

    Update pw.py

--- algorithm/pw.py
+++ algorithm/pw.py
@@ -1,28 +1,36 @@
-import math
-from anytree import AnyNode as Node
 import numpy as np
+from anytree import AnyNode as Node
 
-class FindNextState:
-    def __init__(self, alpha, transitionFunction):
-        self.alpha = alpha
-        self.transitionFunction = transitionFunction
+class InitializeChildren:
+    def __init__(self, actionSpace, transition, getActionPrior):
+        self.actionSpace = actionSpace
+        self.transition = transition
+        self.getActionPrior = getActionPrior
 
-    def __call__(self, stateNode, actionNode):
-        numVisitedAction = actionNode.numVisited
-        k = math.ceil(pow(numVisitedAction, self.alpha))
-        state = stateNode.id
-        action = actionNode.id
-        nextState = self.transitionFunction(state, action)
-        if k > len(actionNode.children):
-            if nextState not in actionNode.children:
-            	nextStateNode = Node(parent = actionNode, id = nextState, numVisited =1)## add action
-        childNodes = [action.id for action in actionNode.children]
-        return childNodes
+    def __call__(self, node):
+        state = list(node.id.values())[0]
+        initActionPrior = self.getActionPrior(state)
 
-def transitionFunction(state, action):
-    nextState = np.array(state)+np.array(action)
-    return list(nextState)
+        for action in self.actionSpace:
+            nextState = self.transition(state, action)
+            actionNode = Node(parent=node, id={"action": action}, numVisited=0, sumValue=0,actionPrior=initActionPrior[action])
+            Node(parent=actionNode, id={"state": nextState}, numVisited=0, sumValue=0,
+                 isExpanded=False)
 
+        return node
+
+class Expand:
+    def __init__(self, isTerminal, initializeChildren):
+        self.isTerminal = isTerminal
+        self.initializeChildren = initializeChildren
+
+    def __call__(self, leafNode):
+        currentState = list(leafNode.id.values())[0]
+        if not self.isTerminal(currentState):
+            leafNode.isExpanded = True
+            leafNode = self.initializeChildren(leafNode)
+
+        return leafNode
 
 class ScoreChild:
     def __init__(self, cInit, cBase):
@@ -36,7 +44,7 @@ class ScoreChild:
             uScore = np.inf
             qScore = 0 
         else:
-            explorationRate = np.log((1 + stateVisitCount + self.cBase) / self.cBase) + self.cInit
+            explorationRate = np.log((1 + stateVisitCount + self.cBase) / self.cBase) + self.cInit 
             uScore = explorationRate * actionPrior * np.sqrt(stateVisitCount) / float(1 + stateActionVisitCount)#selfVisitCount is stateACtionVisitCount
             nextStateValues = [nextState.sumValue for nextState in actionNode.children]
             numNextStateVisits = [nextState.numVisited/actionNode.numVisited for nextState in actionNode.children]
@@ -44,47 +52,38 @@ class ScoreChild:
             qScore = sumNextStateValues / stateActionVisitCount
 
         score = qScore + uScore
-        return score   
+        return score
 
-class SelectAction:
-    def __init__(self, calculateScore):
-        self.calculateScore = calculateScore
+class RollOut:
+    def __init__(self, rolloutPolicy, maxRolloutStep, transitionFunction, rewardFunction, isTerminal, rolloutHeuristic):
+        self.transitionFunction = transitionFunction
+        self.rewardFunction = rewardFunction
+        self.maxRolloutStep = maxRolloutStep
+        self.rolloutPolicy = rolloutPolicy
+        self.isTerminal = isTerminal
+        self.rolloutHeuristic = rolloutHeuristic
 
-    def __call__(self, stateNode, actionNode):
-        scores = [self.calculateScore(stateNode, actionNode) for actionNode in stateNode.children]
-        maxIndex = np.argwhere(scores == np.max(scores)).flatten()
-        selectedChildIndex = np.random.choice(maxIndex)
-        selectedAction = stateNode.children[selectedChildIndex]
-        return selectedAction
+    def __call__(self, leafNode):
+        currentState = list(leafNode.id.values())[0]
+        totalRewardForRollout = 0
 
-class SelectNextState:
-    def __init__(self, selectAction):
-        self.selectAction = selectAction
+        for rolloutStep in range(self.maxRolloutStep):
+            action = self.rolloutPolicy(currentState)
+            totalRewardForRollout += self.rewardFunction(currentState, action)
+            if self.isTerminal(currentState):
+                break
+            nextState = self.transitionFunction(currentState, action)
+            currentState = nextState
 
-    def __call__(self, stateNode, actionNode):
-        selectedAction = self.selectAction(stateNode, actionNode)
-        nextPossibleState = selectedAction.children
-        numNextStateVisits = [nextState.numVisited/actionNode.numVisited for nextState in actionNode.children]
-        nextState = np.random.choice(nextPossibleState,1,numNextStateVisits)
-        return nextState
+        heuristicReward = 0
+        if not self.isTerminal(currentState):
+            heuristicReward = self.rolloutHeuristic(currentState)
+        totalRewardForRollout += heuristicReward
 
-class InitializeChildren:
-    def __init__(self, actionSpace, transition, getActionPrior):
-        self.actionSpace = actionSpace
-        self.transition = transition
-        self.getActionPrior = getActionPrior
-
-    def __call__(self, node):
-        state = list(node.id)
-        initActionPrior = self.getActionPrior(state)
-
-        for action in self.actionSpace:
-            nextStates = self.transition(state, action)
-            Node(parent=node, id=action, numVisited=0, sumValue=0, actionPrior=initActionPrior[action]
-                 ) 
-            for nextState in nextStates:
-                Node(parent=action, id=nextState, numVisited=0, sumValue=0
-                 )
-
-        return node
+        return totalRewardForRollout
+        
+def backup(value, nodeList): #anytree lib
+    for node in nodeList:
+        node.sumValue += value
+        node.numVisited += 1
 
