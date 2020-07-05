aed618e1707fdc8b71a3b3e24c6e1fe051e9b96b
Chenfei1129 <58308092+Chenfei1129@users.noreply.github.com>
2020-07-05 12:51:05 -0700

    Update test according to new pw

--- test/testPW.py
+++ test/testPW.py
@@ -5,7 +5,7 @@ import numpy as np
 from ddt import ddt, data, unpack
 from anytree import AnyNode as Node
 
-from src.algorithm.pw import Expand, InitializeChildren, ScoreChild
+from src.algorithm.pw import Expand, InitializeChildren, ScoreChild, RollOut, backup, RewardFunction, Terminal, SelectAction, SelectNextState
 @ddt
 class TestMCTS(unittest.TestCase):
     def setUp(self):
@@ -22,6 +22,9 @@ class TestMCTS(unittest.TestCase):
         self.c_init = 0
         self.c_base = 1
         self.scoreChild = ScoreChild(self.c_init, self.c_base)
+        self.default_actionPrior = 1/self.num_action_space
+        self.selectAction = SelectAction(self.scoreChild)
+
     @data((3, True, [-1, 1]), (0, True, [-1, 1]), (7, False, None))
     @unpack
     def testExpand(self, state, has_children, child_states):
@@ -44,13 +47,28 @@ class TestMCTS(unittest.TestCase):
         score = self.scoreChild(curr_node, actionChild)
         self.assertEqual(score, groundtruth_score)
 
+    @data((5, [3, 4], [2, 1], [8, 9], [3, 2]))
+    @unpack
+    def testBackup(self, value, prev_sumValues, prev_visit_nums, new_sumValues, new_visit_nums):
+        node_list = []
+        for prev_sumValue, prev_visit_num in zip(prev_sumValues, prev_visit_nums):
+            node_list.append(Node(id={1: 4}, numVisited=prev_visit_num,
+                                  sumValue=prev_sumValue, actionPrior=0.5, isExpanded=False))
+
+        backup(value, node_list)
+        cal_sumValues = [node.sumValue for node in node_list]
+        cal_visit_nums = [node.numVisited for node in node_list]
+        
+        self.assertTrue(np.all(cal_sumValues == new_sumValues))
+        self.assertTrue(np.all(cal_visit_nums == new_visit_nums))
+
     @data((4, 3, 0.125), (3, 4, 0.25))
     @unpack 
     def testRolloutWithoutHeuristic(self, max_rollout_step, init_state, gt_sumValue):
         max_iteration = 1000
 
-        target_state = 6
-        isTerminal = Terminal(target_state)
+        
+        isTerminal = lambda state: state ==6
 
         catch_reward = 1
         step_penalty = 0
@@ -68,6 +86,49 @@ class TestMCTS(unittest.TestCase):
 
         self.assertAlmostEqual(gt_sumValue, calc_sumValue, places=1)
 
+    @data((4, 3, 1.875), (3, 4, 1.75))
+    @unpack 
+    def testRolloutWithHeuristic(self, max_rollout_step, init_state, gt_sumValue):
+        max_iteration = 1000
+
+        target_state = 6
+        isTerminal = Terminal(target_state)
+
+        catch_reward = 1
+        step_penalty = 0
+        reward_func = RewardFunction(step_penalty, catch_reward, isTerminal)
+        rolloutHeuristic = lambda state: 2
+
+        rollout_policy = lambda state: np.random.choice(self.action_space)
+        leaf_node = Node(id={1: init_state}, numVisited=1, sumValue=0, actionPrior=self.default_actionPrior, isExpanded=True)
+        rollout = RollOut(rollout_policy, max_rollout_step, self.transition, reward_func, isTerminal, rolloutHeuristic)
+        stored_reward = []
+        for curr_iter in range(max_iteration):
+            stored_reward.append(rollout(leaf_node))
+        
+        calc_sumValue = np.mean(stored_reward)
+
+        self.assertAlmostEqual(gt_sumValue, calc_sumValue, places=1)
+
+    @data((1,2,3,3,4,3))
+    @unpack
+    def testSelectAction(self, state, action1, action2, nextState1, nextState2, groundTruthAction):
+    	isTerminal = lambda state: state ==4
+    	catch_reward = 1
+    	step_penalty = 0
+    	reward_func = RewardFunction(step_penalty, catch_reward, isTerminal)
+        
+    	stateNode = Node(id ={"state":state}, numVisited =1)
+    	actionNode1 = Node (id ={"action":action1}, numVisited =1, parent = stateNode, actionPrior = 0.5)
+    	#print(actionNode1.actionPrior)
+    	actionNode2 = Node (id ={"action":action2}, numVisited =1, parent = stateNode, actionPrior = 0.5)
+    	nextState1 = Node (id ={"state": nextState1}, numVisited =1, parent = actionNode1, sumValue = 0)
+    	#print(nextState1.sumValue)
+    	nextState2 = Node (id ={"state": nextState2}, numVisited =1, parent = actionNode2, sumValue = 1)
+    	#print(nextState2.sumValue)
+    	calc_selectActionNode = self.selectAction(stateNode)
+    	calc_selectAction = list(calc_selectActionNode.id.values())[0]
+    	self.assertEqual(calc_selectAction,groundTruthAction)
 
 if __name__ == "__main__":
     unittest.main()
